9f948abc0285b586e98d1c40db146d4f5e5838ac
armitag8 <joe.armitage@mail.utoronto.ca>
2019-09-14 17:45:49 -0400

    Improved homepage.

--- docs/home.js
+++ docs/home.js
@@ -4,10 +4,32 @@ const home = `
     I hope you can find some stuff here that helps you succeed in this course.
 </p>
 
+<hr/>
+
 <p>
     I hope to post small examples of code as well as links and general advice over 
     the term. This website is a learning process for me, too, so if there's something
     you'd like to see that isn't here, let me know.
+</p>
+
+<h4>Contact Me</h4>
+<p>
+    The best way to ask as question is with a thoughtful post on 
+    <a href="https://piazza.com/class/k03z36u0gnids">Piazza</a>
+    so that your classmates can also benefit.
+</p>
+<p>
+    If you'd prefer to speak in person, feel free to come to any of my tutorials.
+    <ul>
+        <li>Monday: 10:00-12:00 (BV-498)</li>
+        <li>Thursday: 13:00-15:00 (BV-498)</li>
+        <li>Friday: 11:00-13:00 (BV-469)</li>
+    </ul>
+    As long as we're not too busy, I'd be happy to help.
+</p>
+<p>
+    You could also <a href=javascript:window.location.href=atob('bWFpbHRvOmpvZS5hcm1pdGFnZUBtYWlsLnV0b3JvbnRvLmNh')>email me</a>. 
+    However, unless you have something private to discuss, you'd do better to post to Piazza.
 </p>
 `;
 
