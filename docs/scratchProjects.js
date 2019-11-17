39c5bf1c2491a29a0e1473cbcac345d94f1fadd0
angelazb <angela.zavaletabernuy@mail.utoronto.ca>
2019-11-17 09:15:15 -0500

    Added Angela’s Simon scratch project

--- docs/scratchProjects.js
+++ docs/scratchProjects.js
@@ -1,4 +1,4 @@
 const scratchUsers = ["armitag8"];
-const scratchProjectIDs = ["327664402", "344827107"];
+const scratchProjectIDs = ["327664402", "344827107", "345575510"];
 
 export { scratchUsers, scratchProjectIDs }
